410742e8a7898d1481ab94e5454e537fa7cb2f69
Fernando Girón <fgironpe@gmail.com>
2019-03-18 21:24:14 +0100

    Fin sección 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 
-const app_routes: Routes = [
+const appRoutes: Routes = [
   {path: 'home', component: PortafolioComponent},
   {path: 'about', component: AboutComponent},
   {path: 'item', component: ItemComponent},
@@ -17,7 +17,7 @@ const app_routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(app_routes, {useHash:true})
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   exports: [
     RouterModule
