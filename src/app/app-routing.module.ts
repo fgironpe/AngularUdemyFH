09bf2f80244cb5b24e2ae0d27eba3c3aa408ae34
Fernando Girón <fgironpe@gmail.com>
2019-03-19 20:12:07 +0100

    Fin seccion 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { ItemComponent } from './pages/item/item.component';
 import { AboutComponent } from './pages/about/about.component';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { SearchComponent } from './pages/search/search.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: PortafolioComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'item', component: ItemComponent},
+  {path: 'item/:id', component: ItemComponent},
+  {path: 'search/:termino', component: SearchComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,23 +10,67 @@ export class ProductosService {
 
   cargando = true;
   productos: ProductoInterface[] = [];
+  productosFiltrado: ProductoInterface[] = [];
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
 
     this.cargarProductos();
 
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-fhudemy.firebaseio.com/productos_idx.json').subscribe((resp: ProductoInterface[]) => {
 
-      console.log(resp);
-      this.productos = resp;
+    return new Promise((resolve, reject) => {
 
-      this.cargando = false;
+      this.http.get('https://angular-fhudemy.firebaseio.com/productos_idx.json').subscribe((resp: ProductoInterface[]) => {
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
 
     });
 
+
+  }
+
+  getProducto(id: string) {
+
+    return this.http.get(`https://angular-fhudemy.firebaseio.com/productos/${id}.json`);
+
+  }
+
+  buscarProducto(termino: string) {
+
+    if (this.productos.length === 0) {
+      // Cargar productos
+      this.cargarProductos().then(() => {
+        // Ejecutar después de tener los productos
+        // Aplicar el filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      // Aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+
+  }
+
+  private filtrarProductos(termino: string) {
+
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+
+      const tituloLower = prod.titulo.toLowerCase();
+
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+
+      }
+
+    });
   }
 
 }
